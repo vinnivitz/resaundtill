4a2758956f4d7cdfb979898160854b51fe5d6459
Vinzenz Fuhrmann <vinzenz.f@gmx.de>
2023-03-24 03:07:29 +0100

    Update footer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 
 <main>
     <h1>Willkommen auf dem Reisblog von Till und Resa</h1>
-    <p>Hier könnt ihr immer auf dem laufenden bleiben, wo wir beide uns gerade rumtreiben und was wir spannendes erleben.</p>
+    <p>Hier könnt ihr immer auf dem laufenden bleiben, wo wir beide uns gerade rumtreiben und was wir Spannendes erleben.</p>
 </main>
 
 <footer>Created by VinniVitz with love from Dresden!</footer>
