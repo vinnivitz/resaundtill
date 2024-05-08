855597cb6ea62a5967ee5ebfe538901239f5bfb7
Vinzenz Fuhrmann <vinzenz.f@gmx.de>
2024-05-08 19:31:24 +0200

    directus id -> string

--- src/lib/stores/index.ts
+++ src/lib/stores/index.ts
@@ -1,4 +1,3 @@
-import type { ID } from '@directus/sdk';
 import { writable } from 'svelte/store';
 
-export const imageCache = writable<Map<ID, HTMLImageElement>>(new Map());
+export const imageCache = writable<Map<string, HTMLImageElement>>(new Map());
